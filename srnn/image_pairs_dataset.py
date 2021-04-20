21bc3ef1e74528db31ccf1cfa30da13ebfa554bd
Raphael Nepomuceno <58113327+rphln@users.noreply.github.com>
2021-04-19 21:19:11 -0300

    src: `ImagePairsDataset`.

--- srnn/image_pairs_dataset.py
+++ srnn/image_pairs_dataset.py
@@ -2,9 +2,9 @@ from functools import lru_cache
 from os import PathLike
 from pathlib import Path
 from random import choice
-from typing import Sequence, Tuple
+from sys import stderr
+from typing import Tuple
 
-from PIL import Image
 from torch import Tensor
 from torch.utils.data import Dataset
 from torchvision.datasets.folder import default_loader
@@ -18,16 +18,33 @@ from torchvision.transforms.functional import (
 
 
 class ImagePairsDataset(Dataset):
-    def __init__(self, path: PathLike):
-        self.groups = [group for group in Path(path).glob("*") if group.is_dir()]
+    extensions = {".png", ".jpg"}
+
+    def __init__(self, path: PathLike, lr_size: int, scale: int):
+        self.files = [
+            file for file in Path(path).glob("**/*") if file.suffix in self.extensions
+        ]
+
+        self.lr_size = lr_size
+        self.hr_size = lr_size * scale
+
+        self.crop = RandomCrop(size=self.hr_size)
 
     def __getitem__(self, index: int) -> Tuple[Tensor, Tensor]:
-        files = list(self.groups[index].glob("*.hr"))
+        path: Path = self.files[index]
 
-        hr: Path = choice(files)
-        lr: Path = hr.with_suffix(".lr")
+        try:
+            file = default_loader(path)
+        except Exception as err:
+            print(f"Warning: Invalid file found at `{path}`", file=stderr)
+            raise err
 
-        return to_tensor(default_loader(lr)), to_tensor(default_loader(hr))
+        hr: Tensor = to_tensor(self.crop(file))
+        lr: Tensor = resize(
+            gaussian_blur(hr, kernel_size=3), self.lr_size, InterpolationMode.BICUBIC
+        )
+
+        return lr, hr
 
     def __len__(self):
-        return len(self.groups)
+        return len(self.files)
