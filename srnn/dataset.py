4f9734b3c49aee8d685f1236cd6657209d2c255c
Raphael Nepomuceno <58113327+rphln@users.noreply.github.com>
2021-04-25 11:15:46 -0300

    src: Adicionando `PreloadTensorPairsDataset`.

--- srnn/dataset.py
+++ srnn/dataset.py
@@ -20,4 +20,18 @@ class TensorPairsDataset(Dataset[Tuple[Tensor, Tensor]]):
         return len(self.h5["lr"])
 
 
-__all__ = ("TensorPairsDataset",)
+class PreloadTensorPairsDataset(Dataset[Tuple[Tensor, Tensor]]):
+    def __init__(self, name):
+        self.h5 = h5py.File(name, "r", libver="latest", swmr=True)
+
+        self.lr = torch.from_numpy(self.h5["lr"][:])
+        self.hr = torch.from_numpy(self.h5["hr"][:])
+
+    def __getitem__(self, index: int):
+        return self.lr[index], self.hr[index]
+
+    def __len__(self):
+        return len(self.lr)
+
+
+__all__ = ("TensorPairsDataset", "PreloadTensorPairsDataset")
