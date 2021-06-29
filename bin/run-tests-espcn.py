934ec9a9c1ec517e4c9473b942321dde13a91386
Raphael Nepomuceno <58113327+rphln@users.noreply.github.com>
2021-06-28 22:00:33 -0300

    code: Adicionando códigos dos testes.

---
+++ bin/run-tests-espcn.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env -S poetry run python3
+
+from pathlib import Path
+from typing import Tuple
+
+import click
+import h5py
+import torch
+from kornia.losses import psnr, ssim
+from torch import Tensor
+from torch.nn import Conv2d, Module, PixelShuffle, Sequential, Tanh
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.data.dataset import Dataset
+from tqdm import tqdm
+from tqdm.auto import tqdm
+
+
+# %%
+def conv2d(in_channels, out_channels, kernel_size) -> Module:
+    return Conv2d(in_channels, out_channels, kernel_size, padding=kernel_size // 2)
+
+
+class ESPCN(Module):
+    SCALE = 3
+
+    N0 = 3
+    N1 = 64
+    N2 = 32
+    N3 = N0 * SCALE ** 2
+
+    F1 = 5
+    F2 = 3
+    F3 = 3
+
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = conv2d(
+            in_channels=self.N0, out_channels=self.N1, kernel_size=self.F1
+        )
+        self.conv2 = conv2d(
+            in_channels=self.N1, out_channels=self.N2, kernel_size=self.F2
+        )
+        self.conv3 = conv2d(
+            in_channels=self.N2, out_channels=self.N3, kernel_size=self.F3
+        )
+
+        self.head = Sequential(self.conv1, Tanh(), self.conv2, Tanh())
+        self.tail = Sequential(self.conv3, PixelShuffle(self.SCALE))
+
+        self.sequential = Sequential(self.head, self.tail)
+
+    def forward(self, x):
+        return self.sequential(x)
+
+
+# %%
+class TensorPairsDataset(Dataset[Tuple[Tensor, Tensor]]):
+    def __init__(self, name):
+        h5 = h5py.File(name, "r", libver="latest", swmr=True)
+
+        self.lr = torch.from_numpy(h5["lr"][:])
+        self.hr = torch.from_numpy(h5["hr"][:])
+
+    def __getitem__(self, index: int):
+        return self.lr[index], self.hr[index]
+
+    def __len__(self):
+        return len(self.lr)
+
+
+# %%
+@torch.no_grad()
+def forward(device, state, dataset, batch_size):
+    model = ESPCN().to(device)
+
+    state = torch.load(state)
+    model.load_state_dict(state["model_state_dict"])
+
+    loader = DataLoader(dataset, batch_size, drop_last=True, shuffle=False)
+
+    model.eval()
+
+    mean_psnr = 0.0
+    mean_ssim = 0.0
+
+    for batch, (x, y) in enumerate(loader, start=1):
+        x = x.to(device)
+        y = y.to(device)
+
+        z = model(x).clamp(0.0, 1.0)
+
+        mean_psnr += (psnr(z, y, max_val=1.0).mean().item() - mean_psnr) / batch
+        mean_ssim += (ssim(z, y, window_size=5).mean().item() - mean_ssim) / batch
+
+    return mean_psnr, mean_ssim
+
+
+@click.command()
+@click.option("--device", default="cuda:0", type=torch.device)
+@click.option("--models", required=True, type=Path)
+@click.option("--dataset", required=True, type=Path)
+@click.option("--batch-size", default=32)
+def cli(device, models, dataset, batch_size):
+    dataset = TensorPairsDataset(dataset)
+
+    for state in sorted(models.glob("*.pth")):
+        print(state.stem, *forward(device, state, dataset, batch_size), sep="\t")
+
+
+if __name__ == "__main__":
+    cli()
---
+++ bin/run-tests-srcnn-leaky-relu.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env -S poetry run python3
+
+from pathlib import Path
+from typing import Tuple
+
+import click
+import h5py
+import torch
+from kornia.losses import psnr, ssim
+from torch import Tensor
+from torch.nn import Conv2d, Module, PixelShuffle, Sequential, Tanh
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.data.dataset import Dataset
+from tqdm import tqdm
+from tqdm.auto import tqdm
+
+from torch.nn import Conv2d, Module, init
+from torch.nn.functional import interpolate, mse_loss, relu
+from torchsummary import summary
+from torchvision.transforms.functional import normalize
+
+from torch.nn import Conv2d, Module, init
+from torch.nn.functional import interpolate, l1_loss, mse_loss, relu, leaky_relu
+from torchvision.transforms.functional import normalize
+
+
+# %%
+
+
+def conv2d(in_channels, out_channels, kernel_size) -> Module:
+    return Conv2d(in_channels, out_channels, kernel_size, padding=kernel_size // 2)
+
+
+def init_parameters(module):
+    if isinstance(module, Conv2d):
+        init.normal_(module.weight, std=1e-3)
+        init.constant_(module.bias, val=0e-0)
+
+
+class SRCNN(Module):
+    N0 = 3
+    N1 = 64
+    N2 = 32
+
+    F1 = 9
+    F2 = 5
+    F3 = 5
+
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = conv2d(
+            in_channels=self.N0, out_channels=self.N1, kernel_size=self.F1
+        )
+        self.conv2 = conv2d(
+            in_channels=self.N1, out_channels=self.N2, kernel_size=self.F2
+        )
+        self.conv3 = conv2d(
+            in_channels=self.N2, out_channels=self.N0, kernel_size=self.F3
+        )
+
+    def forward(self, x):
+        x = normalize(x, std=[0.2931, 0.2985, 0.2946], mean=[0.7026, 0.6407, 0.6265])
+
+        x = interpolate(x, scale_factor=3, mode="bicubic", align_corners=False)
+        x = leaky_relu(self.conv1(x), negative_slope=0.01, inplace=True)
+        x = leaky_relu(self.conv2(x), negative_slope=0.01, inplace=True)
+        x = self.conv3(x)
+
+        return x
+
+
+# %%
+class TensorPairsDataset(Dataset[Tuple[Tensor, Tensor]]):
+    def __init__(self, name):
+        h5 = h5py.File(name, "r", libver="latest", swmr=True)
+
+        self.lr = torch.from_numpy(h5["lr"][:])
+        self.hr = torch.from_numpy(h5["hr"][:])
+
+    def __getitem__(self, index: int):
+        return self.lr[index], self.hr[index]
+
+    def __len__(self):
+        return len(self.lr)
+
+
+# %%
+@torch.no_grad()
+def forward(device, state, dataset, batch_size):
+    model = SRCNN().to(device)
+
+    state = torch.load(state)
+    model.load_state_dict(state["model_state_dict"])
+
+    loader = DataLoader(dataset, batch_size, drop_last=True, shuffle=False)
+
+    model.eval()
+
+    mean_psnr = 0.0
+    mean_ssim = 0.0
+
+    for batch, (x, y) in enumerate(loader, start=1):
+        x = x.to(device)
+        y = y.to(device)
+
+        z = model(x).clamp(0.0, 1.0)
+
+        mean_psnr += (psnr(z, y, max_val=1.0).mean().item() - mean_psnr) / batch
+        mean_ssim += (ssim(z, y, window_size=5).mean().item() - mean_ssim) / batch
+
+    return mean_psnr, mean_ssim
+
+
+@click.command()
+@click.option("--device", default="cuda:0", type=torch.device)
+@click.option("--models", required=True, type=Path)
+@click.option("--dataset", required=True, type=Path)
+@click.option("--batch-size", default=32)
+def cli(device, models, dataset, batch_size):
+    dataset = TensorPairsDataset(dataset)
+
+    for state in sorted(models.glob("*.pth")):
+        print(state.stem, *forward(device, state, dataset, batch_size), sep="\t")
+
+
+if __name__ == "__main__":
+    cli()
