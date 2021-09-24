5d4fe068d5e79b97eba87a2c0321163f3f66f313
Raphael Nepomuceno <rphln@users.noreply.github.com>
2021-09-24 14:37:24 -0300

    added: `FilePairsDataset`.

--- cardboard4/__init__.py
+++ cardboard4/__init__.py
@@ -1,5 +1,11 @@
-from cardboard4.data import TensorPairsDataset
+from cardboard4.data import FilePairsDataset, TensorPairsDataset
 from cardboard4.loss import MultiScaleSSIM
 from cardboard4.utils import mean_psnr, mean_ssim
 
-__all__ = ("TensorPairsDataset", "mean_psnr", "mean_ssim", "MultiScaleSSIM")
+__all__ = (
+    "TensorPairsDataset",
+    "FilePairsDataset",
+    "mean_psnr",
+    "mean_ssim",
+    "MultiScaleSSIM",
+)
--- cardboard4/data.py
+++ cardboard4/data.py
@@ -5,6 +5,8 @@ import torch
 from numpy.lib.format import open_memmap
 from torch import Tensor
 from torch.utils.data import Dataset
+from torchvision.datasets.folder import default_loader
+from torchvision.transforms.functional import to_tensor
 
 
 class TensorPairsDataset(Dataset[Tuple[Tensor, Tensor]]):
@@ -15,9 +17,26 @@ class TensorPairsDataset(Dataset[Tuple[Tensor, Tensor]]):
         self.lr = torch.from_numpy(open_memmap(lr, mode="c"))
         self.hr = torch.from_numpy(open_memmap(hr, mode="c"))
 
-    def __getitem__(self, index: int) -> (Tensor, Tensor):
+    def __getitem__(self, index: int) -> Tuple[Tensor, Tensor]:
         lr = self.lr[index]
         hr = self.hr[index]
+
+        return lr, hr
+
+    def __len__(self):
+        return len(self.lr)
+
+
+class FilePairsDataset(Dataset[Tuple[Tensor, Tensor]]):
+    def __init__(self, path: Path):
+        super().__init__()
+
+        self.lr = sorted(path.glob("**/*.LR.png"))
+        self.hr = sorted(path.glob("**/*.HR.png"))
+
+    def __getitem__(self, index: int) -> Tuple[Tensor, Tensor]:
+        lr = to_tensor(default_loader(self.lr[index]))
+        hr = to_tensor(default_loader(self.hr[index]))
 
         return lr, hr
 
